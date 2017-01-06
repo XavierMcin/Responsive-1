126381aa0ad3616fe7fb620fa92f749782a72c63
Xavier McIntosh <xavier2794@gmail.com>
2017-01-06 15:52:39 -0500

    update

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ $(function(){
 	$(window).scroll(function(event){
 		var target = $(this).scrollTop();
 
-		if (target >= 30) {
+		if (target >= 60) {
 			$('.fixedMenu').addClass('stuck');
 			$('.carousel').addClass('stuck');
 			$('.farLeft nav ul li a').addClass('stuck');
@@ -12,7 +12,7 @@ $(function(){
 			$('.farRight nav ul li a').addClass('stuck');
 		}
 
-		if (target < 100) {
+		if (target < 60) {
 			$('.fixedMenu').removeClass('stuck');
 			$('.carousel').removeClass('stuck');
 			$('.farLeft nav ul li a').removeClass('stuck');
@@ -27,4 +27,17 @@ $(function(){
 
 
 	});
+});
+
+$(function(){
+	$('.hamMenu').on('click', function(){
+		$('.hamMenu').toggleClass('sideMenu');
+		$('.hamMenu').toggleClass('menuSlide');
+		$('.menuOption').toggleClass('slide');
+	});
+});
+
+
+$(function(){
+	$('.carousel-inner1').flickity();
 });
